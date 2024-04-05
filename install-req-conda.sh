c87491bebd7a7c32727a82ce5287bfa247005bea
root <root@ml1project-shared--20240323-223216.us-central1-a.c.ml1hecmontreal.internal>
2024-04-05 21:05:49 +0000

    add conda install shell script

---
+++ install-req-conda.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Conda package installation script
+
+# List of packages to install
+PACKAGES=(
+  emoji
+  fuzzywuzzy
+  gensim
+  matplotlib
+  nltk
+  numpy
+  pandas
+  praw
+  pyLDAvis
+  scipy
+  spacy
+  tqdm
+  transformers
+  wordcloud
+  yahoo_fin
+  yfinance
+)
+
+echo "Starting installation of Python packages using conda..."
+
+# Loop through the packages and install each one
+for PACKAGE in "${PACKAGES[@]}"; do
+  echo "Installing $PACKAGE..."
+  conda install "$PACKAGE" -y
+done
+
+echo "Installation of packages completed."
